a445e4341f38f442384f157d76a978ce98d7909d
Jackrayallday <jackray3111@gmail.com>
2026-06-19 23:45:57 -0700

    Info received
    info received and now need to design the page to show that info

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from "../context/AuthContext";
-import {Navigate, replace} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export default function Home() {
     const { user, isLoading } = useAuth();
